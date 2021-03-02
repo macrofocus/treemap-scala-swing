0bd6c96bcd71cd3c097e80c210a05d3839e2cc3c
Luc Girardin <luc.girardin@ethz.ch>
2021-03-02 18:07:48 +0100

    TreeMap Scala/Swing

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,3 +35,17 @@ dependencies {
 application {
     mainClass.set("Demo")
 }
+
+distributions {
+    main {
+//        distributionBaseName.set("someName")
+        contents {
+//            from("TreeMap API for Scale-Swing Developer Guide.pdf")
+        }
+
+        contents {
+            from("../treemap/build/dokka/html/")
+            into("dokka")
+        }
+    }
+}
