c0a4a010fb0a6dcd76756f3d2eace99e910a4a0b
Luc Girardin <luc.girardin@macrofocus.com>
2024-01-14 00:04:32 +0100

    Java 21

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ configurations.all {
 }
 
 dependencies {
-    implementation("org.scala-lang:scala-library:2.13.10")
+    implementation("org.scala-lang:scala-library:2.13.12")
 
     val localDependencies: String? by project
     if (localDependencies != null && localDependencies.toBoolean()) {
